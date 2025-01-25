e9d0c8b21805960cfe76c4ed443478d67153fdcd
ABDERRAHMANE LAOURF <152749612+abderrahmane-laourf@users.noreply.github.com>
2025-01-25 22:29:53 +0100

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,4 @@
-import { questions } from './question.js'; // 
+import { questions } from './question.js'; 
 
 const answerOptions = document.querySelector(".answer-options");
 const nextButton = document.querySelector(".next");
